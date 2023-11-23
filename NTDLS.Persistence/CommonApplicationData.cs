8d1146e2007c4036f53ccba0785647c1dedb3c8d
Josh Patterson <josh@ntdls.com>
2023-11-23 11:13:20 -0500

    Added optional serialization encryption provider.

--- NTDLS.Persistence/CommonApplicationData.cs
+++ NTDLS.Persistence/CommonApplicationData.cs
@@ -1,4 +1,7 @@
-﻿namespace NTDLS.Persistence
+﻿using Newtonsoft.Json;
+using static NTDLS.Persistence.PathPersistence;
+
+namespace NTDLS.Persistence
 {
     /// <summary>
     /// Reads/writes to the directory that serves as a common repository for application-specific data that is used by all users.
@@ -8,19 +11,25 @@
         /// <summary>
         /// Json serializes an object and saves it to the disk.
         /// </summary>
-        public static void SaveToDisk<T>(string applicationName, T obj)
-            => PathPersistence.SaveToDisk<T>(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, obj);
+        public static void SaveToDisk<T>(string applicationName, T obj, EncryptionProvider? encryptionProvider = null)
+            => PathPersistence.SaveToDisk<T>(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, obj, encryptionProvider);
+
+        /// <summary>
+        /// Json serializes an object and saves it to the disk.
+        /// </summary>
+        public static void SaveToDisk<T>(string applicationName, T obj, JsonSerializerSettings settings, EncryptionProvider? encryptionProvider = null)
+            => PathPersistence.SaveToDisk<T>(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, obj, settings, encryptionProvider);
 
         /// <summary>
         /// Loads the object from disk, deserializes it ans returns the object.
         /// </summary>
-        public static T? LoadFromDisk<T>(string applicationName)
-            => PathPersistence.LoadFromDisk<T>(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName);
+        public static T? LoadFromDisk<T>(string applicationName, EncryptionProvider? encryptionProvider = null)
+            => PathPersistence.LoadFromDisk<T>(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, encryptionProvider);
 
         /// <summary>
         /// Loads the object from disk, deserializes it ans returns the object.
         /// </summary>
-        public static T LoadFromDisk<T>(string applicationName, T defaultResult)
-            => PathPersistence.LoadFromDisk(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, defaultResult);
+        public static T LoadFromDisk<T>(string applicationName, T defaultResult, EncryptionProvider? encryptionProvider = null)
+            => PathPersistence.LoadFromDisk(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), applicationName, defaultResult, encryptionProvider);
     }
 }
--- NTDLS.Persistence/PathPersistence.cs
+++ NTDLS.Persistence/PathPersistence.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System.Text;
 
 namespace NTDLS.Persistence
 {
@@ -8,9 +9,16 @@ namespace NTDLS.Persistence
     public class PathPersistence
     {
         /// <summary>
+        /// Delegate of encryption/decryption to use used after serialization and before deserialization.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public delegate byte[] EncryptionProvider(byte[] bytes);
+
+        /// <summary>
         /// Json serializes an object and saves it to the disk.
         /// </summary>
-        public static void SaveToDisk<T>(string directoryPath, string applicationName, T obj)
+        public static void SaveToDisk<T>(string directoryPath, string applicationName, T obj, EncryptionProvider? encryptionProvider = null)
         {
             var dataPath = Path.Combine(directoryPath, applicationName);
 
@@ -22,15 +30,47 @@ namespace NTDLS.Persistence
             var type = typeof(T);
             string typeName = type.IsGenericType ? type.GetGenericArguments()[0].Name : type.Name;
 
-            var jsonText = JsonConvert.SerializeObject(obj, Formatting.Indented);
+            var jsonBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, Formatting.Indented));
             string dataFilePath = Path.Combine(dataPath, $"{typeName}.json");
-            File.WriteAllText(dataFilePath, jsonText);
+
+            if (encryptionProvider != null)
+            {
+                jsonBytes = encryptionProvider(jsonBytes);
+            }
+
+            File.WriteAllBytes(dataFilePath, jsonBytes);
+        }
+
+        /// <summary>
+        /// Json serializes an object and saves it to the disk.
+        /// </summary>
+        public static void SaveToDisk<T>(string directoryPath, string applicationName, T obj, JsonSerializerSettings settings, EncryptionProvider? encryptionProvider = null)
+        {
+            var dataPath = Path.Combine(directoryPath, applicationName);
+
+            if (!Directory.Exists(dataPath))
+            {
+                Directory.CreateDirectory(dataPath);
+            }
+
+            var type = typeof(T);
+            string typeName = type.IsGenericType ? type.GetGenericArguments()[0].Name : type.Name;
+
+            var jsonBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(obj, settings));
+            string dataFilePath = Path.Combine(dataPath, $"{typeName}.json");
+
+            if (encryptionProvider != null)
+            {
+                jsonBytes = encryptionProvider(jsonBytes);
+            }
+
+            File.WriteAllBytes(dataFilePath, jsonBytes);
         }
 
         /// <summary>
         /// Loads the object from disk, deserializes it ans returns the object.
         /// </summary>
-        public static T? LoadFromDisk<T>(string directoryPath, string applicationName)
+        public static T? LoadFromDisk<T>(string directoryPath, string applicationName, EncryptionProvider? encryptionProvider = null)
         {
             var dataPath = Path.Combine(directoryPath, applicationName);
 
@@ -45,8 +85,14 @@ namespace NTDLS.Persistence
             string dataFilePath = Path.Combine(dataPath, $"{typeName}.json");
             if (File.Exists(dataFilePath))
             {
-                var jsonText = File.ReadAllText(dataFilePath);
-                var obj = JsonConvert.DeserializeObject<T>(jsonText);
+                var jsonBytes = File.ReadAllBytes(dataFilePath);
+
+                if (encryptionProvider != null)
+                {
+                    jsonBytes = encryptionProvider(jsonBytes);
+                }
+
+                var obj = JsonConvert.DeserializeObject<T>(Encoding.UTF8.GetString(jsonBytes));
                 return obj;
             }
             return default;
@@ -55,9 +101,9 @@ namespace NTDLS.Persistence
         /// <summary>
         /// Loads the object from disk, deserializes it ans returns the object.
         /// </summary>
-        public static T LoadFromDisk<T>(string directoryPath, string applicationName, T defaultResult)
+        public static T LoadFromDisk<T>(string directoryPath, string applicationName, T defaultResult, EncryptionProvider? encryptionProvider = null)
         {
-            var result = LoadFromDisk<T>(directoryPath, applicationName);
+            var result = LoadFromDisk<T>(directoryPath, applicationName, encryptionProvider);
             if (result == null)
             {
                 return defaultResult;
